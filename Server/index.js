a2fc5b86edd8862cdf6c8fda0dbe8b4dfd82b2c4
ShockRate <blackhound05@yahoo.gr>
2021-01-31 19:10:48 +0200

    Some testing changes

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config();
 
 
-
+//Test Change
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
